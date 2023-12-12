eceab56333e230d14eecb16e9e55f4912daffa17
Leonardo Moreira <leonardomoreiravalerio@gmail.com>
2023-12-12 16:42:10 -0300

    Criei no meu views um dicionário onde passo os nomes e as descricção de cada card e fiz um for no meu index html para cada item do meu dicionário me gerar um card novo, essa não é a melhor maneira de se fazer, entao para isso abri criei um banco de dados pelo models para daí sim adicionar os dados que quero

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {1:{'nome': 'foto1', 'autor':'leo'},2:{'nome':'foto2', 'autor': 'gabi'}}
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
