64b4a79141b510258cea929a8672c52121f16b43
Leonardo Moreira <leonardomoreiravalerio@gmail.com>
2023-12-24 14:09:13 -0300

    adicionei no banco de dados a data de cada publicação e ordenei as fotografias pelas datas mais recentes

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 def index(request):
-    fotografia = Fotografia.objects.filter(publicada=True)
+    fotografia = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
     return render(request, 'galeria/index.html', {'cards': fotografia})
 
 def imagem(request, foto_id):
